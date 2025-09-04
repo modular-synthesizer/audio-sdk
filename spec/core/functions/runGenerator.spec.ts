e7688640ab2c1bcbd0df580ade506da8fd136749
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-09-04 11:40:50 +0200

    Add tests for the failures

--- spec/core/functions/runGenerator.spec.ts
+++ spec/core/functions/runGenerator.spec.ts
@@ -3,12 +3,28 @@ import type { NodeGenerator } from "../../../src/core/business/NodeGenerator.typ
 import { runGenerator } from "../../../src/core/functions/runGenerator.js";
 
 describe("runGenerator", () => {
-  const generator: NodeGenerator = {
-    name: 'testGenerator',
-    id: 'testGeneratorId',
-    code: 'return new OscillatorNode(context, {frequency: 440})'
-  }
   it("Correctly runs a generator", async () => {
+    const generator: NodeGenerator = {
+      name: 'testGenerator',
+      id: 'testGeneratorId',
+      code: 'return new OscillatorNode(context, {frequency: 440})'
+    }
     expect(await runGenerator(generator, new AudioContext())).toBeInstanceOf(OscillatorNode)
+  })
+  it("correctly fails when a generator properly throws an error", async () => {
+    const generator: NodeGenerator = {
+      name: 'testGenerator',
+      id: 'testGeneratorId',
+      code: 'throw "test"'
+    }
+    expect(await runGenerator(generator, new AudioContext())).toBeInstanceOf(GainNode)
+  })
+  it("correctly fails when there is a syntax error", async () => {
+    const generator: NodeGenerator = {
+      name: 'testGenerator',
+      id: 'testGeneratorId',
+      code: 'syntax error everywhere'
+    }
+    expect(await runGenerator(generator, new AudioContext())).toBeInstanceOf(GainNode)
   })
 })

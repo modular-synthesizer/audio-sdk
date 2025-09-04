f807d2effbb7b6d8d04bd9351e9ff10e5722aefa
Vincent COURTOIS <vincent.courtois@ecov.fr>
2025-09-04 11:57:01 +0200

    Add the last tests for generators instanciation, testing parameters in case of error

--- spec/core/functions/runGenerator.spec.ts
+++ spec/core/functions/runGenerator.spec.ts
@@ -11,27 +11,43 @@ describe("runGenerator", () => {
     }
     const audioNode: AudioNode = await runGenerator(generator, new AudioContext())
 
-    it("Correctly runs a generator", async () => {
+    it("Correctly runs a generator", () => {
       expect(audioNode).toBeInstanceOf(OscillatorNode)
     })
     it("Has set the correct value for the frequency", () => {
       expect((audioNode as OscillatorNode).frequency.value).toEqual(42)
     })
   })
-  it("correctly fails when a generator properly throws an error", async () => {
-    const generator: NodeGenerator = {
-      name: 'testGenerator',
-      id: 'testGeneratorId',
-      code: 'throw "test"'
-    }
-    expect(await runGenerator(generator, new AudioContext())).toBeInstanceOf(GainNode)
-  })
-  it("correctly fails when there is a syntax error", async () => {
-    const generator: NodeGenerator = {
-      name: 'testGenerator',
-      id: 'testGeneratorId',
-      code: 'syntax error everywhere'
-    }
-    expect(await runGenerator(generator, new AudioContext())).toBeInstanceOf(GainNode)
+  describe("alternative cases", () => {
+    describe("When the code is supposed to fail with an exception", async () => {
+      const generator: NodeGenerator = {
+        name: 'testGenerator',
+        id: 'testGeneratorId',
+        code: 'throw "test"'
+      }
+      const audioNode: AudioNode = await runGenerator(generator, new AudioContext())
+
+      it("Returns a classic gain node", () => {
+        expect(audioNode).toBeInstanceOf(GainNode)
+      })
+      it("Returns a gain node with a gain set to 1", () => {
+        expect((audioNode as GainNode).gain.value).toEqual(1)
+      })
+    })
+    describe("When the code fails unexpectedly, for example from a syntax error", async () => {
+      const generator: NodeGenerator = {
+        name: 'testGenerator',
+        id: 'testGeneratorId',
+        code: 'strange syntax error'
+      }
+      const audioNode: AudioNode = await runGenerator(generator, new AudioContext())
+
+      it("Returns a classic gain node", () => {
+        expect(audioNode).toBeInstanceOf(GainNode)
+      })
+      it("Returns a gain node with a gain set to 1", () => {
+        expect((audioNode as GainNode).gain.value).toEqual(1  )
+      })
+    })
   })
 })
